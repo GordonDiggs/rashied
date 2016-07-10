20694e93ec9837c9a01e3a9f479ca0f4bbf1d3b2
GordonDiggs <gordon@gordondiggs.com>
2016-07-10 14:59:22 -0400

    skeleton

--- src/rashied.cr
+++ src/rashied.cr
@@ -1,10 +1,20 @@
 require "./rashied/*"
 require "kemal"
 
-get "/:term" do |req|
-  term = req.params.url["term"]
+get "/:term" do |env|
+  term = env.params.url["term"]
 
-  "You want #{term}?"
+  url = Rashied::ImageFinder.new(term).url
+
+  if url
+    env.redirect(url)
+  else
+    env.response.status_code = 404
+  end
+end
+
+get "/" do
+  "Specify a term"
 end
 
 Kemal.run
